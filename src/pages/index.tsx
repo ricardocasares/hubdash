89375daa70686b8f0580f2c1538a9a49358209e8
Ricardo Casares <ricardocasares@gmail.com>
2022-05-13 16:39:07 +0200

    fix: avatars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,11 @@ export type WorkflowRun = {
 
 export function WorkflowRun(props: WorkflowRun) {
   return <div className={`card ${props.status} ${props.conclusion}`}>
+    <Img src={props.avatar} alt={props.actor} width="20" height="20" />
     <p>{props.name}</p>
     <p>{props.branch}</p>
     <p>status: {props.status}</p>
     <p>conclusion: {props.conclusion}</p>
-    <Img src={props.avatar} alt={props.actor} width="20" height="20" />
   </div>;
 }
 
