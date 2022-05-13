2969c8fcafaad95dd46cf24c5c633dded60e5bf3
Ricardo Casares <ricardocasares@gmail.com>
2022-05-13 23:32:59 +0200

    fix: form improvements

--- src/components/FormAddRepository/index.tsx
+++ src/components/FormAddRepository/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { useGlobalState } from "@/hooks/useGlobalState";
+import css from './styles.module.css';
 
 export function FormAddRepository() {
   const [payload, setPayload] = useState("");
   const { dispatch } = useGlobalState();
 
-  return <div>
+  return <div className={css.form}>
     <input type="text" placeholder="owner/repo" onChange={e => setPayload(e.target.value)} />
-    <button onClick={() => dispatch({ type: "ADD_REPO", payload })}>Add</button>
+    <input type="button" onClick={() => dispatch({ type: "ADD_REPO", payload })} value="Add" />
   </div>;
 }
