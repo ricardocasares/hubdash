e9e7cd690d18b8cf4e34a1b67db12d147faaef09
Ricardo Casares <ricardocasares@gmail.com>
2022-05-14 17:51:51 +0200

    fix: disable submit when no input

--- src/components/FormAddRepository/index.tsx
+++ src/components/FormAddRepository/index.tsx
@@ -5,6 +5,7 @@ import css from './styles.module.css';
 export function FormAddRepository() {
   const [payload, setPayload] = useState("");
   const { dispatch } = useGlobalState();
+  const disabled = payload.length < 3;
 
   const onClick = () => {
     dispatch({ type: "ADD_REPO", payload });
@@ -13,6 +14,6 @@ export function FormAddRepository() {
 
   return <div className={css.form}>
     <input type="text" placeholder="owner/repo" onChange={e => setPayload(e.target.value)} value={payload} />
-    <input type="button" onClick={onClick} value="Add" />
+    <input type="button" onClick={onClick} value="Add" disabled={disabled} />
   </div>;
 }
